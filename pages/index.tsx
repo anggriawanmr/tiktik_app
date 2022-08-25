7a63bf5fb5aa4ccaac04a4ec6ff5537b37d99472
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-08-25 14:52:31 +0700

    Adding types.d.ts :sparkles:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
 import type { NextPage } from 'next';
 import axios from 'axios';
 
-const Home: NextPage = () => {
+// interface IProps {
+//   videos:
+// }
+
+const Home: NextPage = ({ videos }) => {
+  console.log(videos);
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`);
-
-  console.log(response.data.name);
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
 
   return {
-    props: {},
+    props: {
+      videos: data,
+    },
   };
 };
 
