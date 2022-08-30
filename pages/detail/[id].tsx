1ffbb6e86f26194d47da7a9d5a7ab1384c4a6609
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-08-30 15:12:42 +0700

    Adding LikeButton and Comments component :sparkles:

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -10,6 +10,9 @@ import axios from 'axios';
 
 import { BASE_URL } from '../../utils';
 import { Video } from '../../types';
+import useAuthStore from '../../store/authStore';
+import Comments from '../../components/Comments';
+import LikeButton from '../../components/LikeButton';
 
 interface Iprops {
   postDetails: Video;
@@ -21,6 +24,7 @@ const Detail = ({ postDetails }: Iprops) => {
   const [isVideoMuted, setIsVideoMuted] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
   const router = useRouter();
+  const { userProfile } = useAuthStore();
 
   const onVideoClick = () => {
     if (playing) {
@@ -75,6 +79,44 @@ const Detail = ({ postDetails }: Iprops) => {
               <HiVolumeUp className="text-white text-2xl lg:text-4xl" />
             </button>
           )}
+        </div>
+      </div>
+      <div className="relative w-[1000px] md:w-[900px] lg:w-[700px]">
+        <div className="lg:mt-20 mt-10 ">
+          <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+            <div className="ml-4 md:w-20 md:h-20 w-16 h-16">
+              <Link href="/">
+                <>
+                  <Image
+                    width={62}
+                    height={62}
+                    className="rounded-full"
+                    src={post.postedBy.image}
+                    alt="profile photo"
+                    layout="responsive"
+                  />
+                </>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <div className="mt-3 flex flex-col gap-2">
+                  <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                    {post.postedBy.userName} {` `}
+                    <GoVerified className="text-blue-400 text-md" />
+                  </p>
+                  <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                    {post.postedBy.userName}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          </div>
+
+          <p className="px-10 text-lg text-gray-600">{post.caption}</p>
+
+          <div className="mt-10 px-10">{userProfile && <LikeButton />}</div>
+          <Comments />
         </div>
       </div>
     </div>
