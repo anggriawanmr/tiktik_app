ab3a250efd2f8175f78e332e8f28e600e9f72592
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-08-31 09:57:50 +0700

    Editing Comments component :hammer:

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { GoVerified } from 'react-icons/go';
+
+import useAuthStore from '../store/authStore';
+import NoResults from './NoResults';
 
 const Comments = () => {
-  return <div>Comments</div>;
+  const comments = [];
+
+  return (
+    <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#f8f8f8] border-p-2 lg:pb-0 pb-[100px]">
+      <div className="overflow-scroll lg:h-[475px]">
+        {comments.length ? (
+          <div>Videos</div>
+        ) : (
+          <NoResults text="No comments yet! Be the first one to add a comment." />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
