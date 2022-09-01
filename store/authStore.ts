e38fb0981ba53ee4edfef329027491a265c16c8b
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-01 11:43:03 +0700

    Adding users api :sparkles:

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,6 +1,7 @@
 import create from 'zustand';
 import { persist } from 'zustand/middleware';
 import axios from 'axios';
+import { BASE_URL } from '../utils';
 
 const authStore = (set: any) => ({
   userProfile: null,
@@ -8,6 +9,10 @@ const authStore = (set: any) => ({
 
   addUser: (user: any) => set({ userProfile: user }),
   removeUser: () => set({ userProfile: null }),
+
+  fetchAllUsers: async () => {
+    const response = await axios.get(`${BASE_URL}/api/users`);
+  },
 });
 
 const useAuthStore = create(
